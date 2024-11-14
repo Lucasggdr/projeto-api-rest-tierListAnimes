1992daf753a2915b2a6ef7f5089dc0b2feae2c8f
Lucas Gomes <lucasgomes150104@gmail.com>
2024-11-14 20:15:50 -0300

    Update TierListController.java

--- src/main/java/me/dio/controller/UserController.java
+++ src/main/java/me/dio/controller/UserController.java
@@ -1,36 +1,27 @@
-package me.dio.controller;
-
-import me.dio.domain.model.User;
-import me.dio.service.UserService;
-import org.springframework.http.ResponseEntity;
+// AnimeController.java
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
+import java.util.List;
 
 @RestController
-@RequestMapping("/users")
-public class UserController {
+@RequestMapping("/api/animes")
+public class AnimeController {
+    @Autowired
+    private AnimeService animeService;
 
-    private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id) {
-       var user = userService.findById(id);
-       return ResponseEntity.ok(user);
+    @GetMapping
+    public List<Anime> getAnimes() {
+        return animeService.getAllAnimes();
     }
 
     @PostMapping
-    public ResponseEntity<User> create(@RequestBody User userToCreate) {
-        var userCreated = userService.create(userToCreate);
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(userCreated.getId())
-                .toUri();
-        return ResponseEntity.created(location).body(userCreated);
+    public Anime addAnime(@RequestBody Anime anime) {
+        return animeService.addAnime(anime);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAnime(@PathVariable Long id) {
+        animeService.deleteAnime(id);
     }
 }
